4734544ccf0d5c89c6a644ac1d1741e47eb95387
johnleung528 <39686681+johnleung528@users.noreply.github.com>
2018-06-28 18:19:46 +0800

    Add files via upload

--- src/app/Win.js
+++ src/app/Win.js
@@ -7,16 +7,45 @@ import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuIcon from '@material-ui/icons/Menu';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+
+const getInfo = {
+  rn: {
+    name: 'RN',
+    description: '主動計劃並實踐，向特定對象打開福音話題之人次'
+  },
+  mc: {
+    name: 'MC',
+    description: '隨機佈道打開福音話題之人次'
+  },
+  inv: {
+    name: 'INV',
+    description: '邀請對象接受基督成為救主之人次'
+  },
+  prc: {
+    name: 'PRC',
+    description: '接受基督成為救主之人數'
+  },
+  hsb: {
+    name: 'HSB',
+    description: '分享被聖靈充滿真理之對象人次'
+  },
+  ahs: {
+    name: 'AHS',
+    description: '願意即時禱告被聖靈充滿之人次'
+  },
+}
 
 class Win extends Component {
   constructor (props) {
     super(props);
     this.state = {
       open: false,
+      takeMethod: 'add',
+      collect_tab: 'Win',
+      week: '2018-06-25',
+      peopleGroup: 'local',
       stat: {
         rn: 0,
         mc: 0,
@@ -70,31 +99,35 @@ class Win extends Component {
   render() {
 
     return (
-
-
         <div className="win">
           <Paper className="win-header">
             <div className="header-box">
-              <h2>Hi! John</h2>
-              <Button onClick={this.toggleDrawer} style={{display: 'block', margin: '0px', padding: '0px', width: '15px', textAlign: 'left', fontSize: '1.5em'}}><MenuIcon style={{color: 'white', fontSize: '1.3em', margin: '0'}}/>{this.state.stat.rn}</Button>
+              <h3 style={{display: 'inline-block', position: 'relative', width: '50%', marginTop: '0.5em'}}>Hi! John</h3>
+              <h3 style={{display: 'inline-block', position: 'relative', width: '50%'}}>{this.state.week}</h3>
             </div>
             <div className="header-box">
-              <h2 style={{marginRight: "30%"}}>Hi!</h2>
-                <Select
-                  value={this.state.stat.rn}
-                  onChange={this.handleChange}
-                  className='win-select'
-                  inputProps={{
-                     name: 'People Group',
-                     id: 'people-group',
-                  }}
-                >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
+              <button
+                onClick={this.toggleDrawer}
+                style={{display: 'block', position: 'relative', float: 'left', boxSizing: 'border-box', border: 'none', height: '100%', width: '50%', padding: '0', margin: '0', backgroundColor: 'red'}}
+                className='ripple'
+              >
+                <MenuIcon
+                 style={{display: 'block', position: 'relative', fontSize: '3em', padding: '0', margin: '0', height: '100%', float: 'left'}} />  {this.state.stat.rn}
+              </button>
 
-
+              <Select
+                value={this.state.stat.rn}
+                onChange={this.handleChange}
+                inputProps={{
+                   name: 'People Group',
+                   id: 'people-group',
+                }}
+                style={{display: 'inline-block', position: 'relative', width: '50%', height: '90%'}}
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
             </div>
           </Paper>
 
@@ -109,7 +142,7 @@ class Win extends Component {
 
           <section>
             RN
-            <button onClick={this.handleMinus} >minus</button>
+            <button onClick={this.handleMinus} className='ripple'>minus</button>
             <TextField
               id="number"
               value={this.state.stat.rn}
@@ -119,7 +152,6 @@ class Win extends Component {
               margin="dense"
             />
             <button onClick={this.handlePlus} >plus</button>
-
           </section>
         </div>
     );
